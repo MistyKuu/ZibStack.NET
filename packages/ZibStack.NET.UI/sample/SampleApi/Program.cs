c998ae7336f44758ea356e7ae15f972ff2d00e48
Zbigniew Szepczyński <mistyksu@gmail.com>
2026-04-07 06:16:58 +0200

    Remove ZibStack.NET.UI package (moved to separate non-open-source repo)

--- packages/ZibStack.NET.UI/sample/SampleApi/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-var app = builder.Build();
-app.MapControllers();
-app.Run();
